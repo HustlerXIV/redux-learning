1818602ec3151c0fc1725e4a43954115204c0b17
Napat Jaiyim <napat@soilfish.co>
2022-09-15 22:32:25 +0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser, deleteUser, updateUsername } from "./features/Users";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [newUsername, setNewUserName] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="addUser">
+        <h2 style={{ color: "#fff" }}>Napat Jaiyim</h2>
+        <input
+          className="customInput"
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          className="customInput"
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button
+          className="addButton"
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name: name,
+                username: username,
+              })
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add User
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div className="listContainer">
+              <div className="infoContainer">
+                <div style={{ display: "flex", gap: "5px" }}>
+                  <p>{user.id}.</p>
+                  <p>{user.name}</p>
+                  <p>{user.username}</p>
+                </div>
+              </div>
+              <input
+                className="customInput"
+                type="text"
+                placeholder="New Username..."
+                onChange={(event) => {
+                  setNewUserName(event.target.value);
+                }}
+              />
+              <button
+                className="updateButton"
+                onClick={() => {
+                  dispatch(
+                    updateUsername({ id: user.id, username: newUsername })
+                  );
+                }}
+              >
+                Update Username
+              </button>
+              <button
+                className="deleteButton"
+                onClick={() => {
+                  dispatch(deleteUser({ id: user.id }));
+                }}
+              >
+                Delete User
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
